6ffc4d7fc78b69f4407a3be0eb594e1ae4e45632
MNKE <112430728+Sir-Monke@users.noreply.github.com>
2024-07-21 14:54:11 +0100

    Update Program.cs

--- Chat-App-Server-V1/Program.cs
+++ Chat-App-Server-V1/Program.cs
@@ -1,6 +1,43 @@
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠀⠀⠀⢶⣦⣄⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣤⡴⢀⣠⣴⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣦⣤⣀⣀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣤⣴⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣌⡙⠿⣿⣦⣄⡀⠀⠀
+⠀⠀⣀⣤⣾⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣉⠻⣿⣶⠀
+⢀⣶⣿⢟⣽⣿⣿⣿⡿⠿⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠛⠿⠿⠿⣿⣿⣿⣿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⢿⣿⣿⡿⠿⠿⠿⠟⠛⠋⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⢿⣿⣿⣷⣜⠃⠀
+⠀⠙⢡⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⠋⠁⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣁⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣷⡄
+⠀⠀⠈⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⢁⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣈⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣴⣶⣶⣶⣶⣶⣶⣦⣶⣤⣤⣤⣤⣤⣤⣤⣤⣀⠀⠘⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⠋⠁⣠⣤⣤⣶⣶⣶⣶⣶⣶⣾⣿⣿⣾⣿⣿⣿⣿⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⡟⠋⠁⣼⣿⣿⣿⣿⣿⡟⢻⣿⣿⡍⣿⣿⣏⠙⠛⠃⠀⢹⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠉⠀⠀⠉⢁⣼⣿⣿⢁⣿⣿⣿⣿⣿⣿⣿⠛⣿⣿⠤⠉⠛⢻⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣟⣁⣀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠈⢿⣿⣷⠀⠀⠀⢸⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⠀⠀⠀⣾⣿⠿⠁⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⣀⠀⢀⣿⣿⣿⣦⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⠛⠀⠙⠿⣿⣿⣷⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣯⣶⣶⣶⣽⣿⣿⡀⠀⠘⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠀⢀⣼⣿⣷⡶⠶⠶⠿⠿⠿⠿⠿⠿⢿⣿⠿⠷⠶⣿⣿⣿⠿⠋⠀⠙⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⡿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣾⡟⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⠏⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⡀⠀⠀⠀⠀⠀⢀⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠋⠀⠀⠀⠀⠀⣾⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⠟⠀⠀⠀⠀⠀⠀⣼⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣾⡿⠿⠛⠀⠀⠀⠀⠀⠀⠀⢠⣿⡟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣤⣤⣤⣴⣶⣶⣿⣿⣿⠿⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣷⣾⣶⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣾⣿⣷⣾⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣭⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+*/
+
+
+// thank god for stack overflow bro i would be done for if that shit didnt exist
+
 using System;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +55,28 @@ class ServerTCPApp
 {
     public static TcpListener Server;
 
+    // to display a few of the Client Messages below menu
+    public static List<string> RecentMessages = new List<string>();
+
     class ServerTCPHandler
     {
+        // Server Info
         private static readonly IPAddress LocalAddr = GetServerIPv4();
         private static readonly int Port = 80;
         private static bool ServerState = false;
 
-        private static IPAddress GetServerIPv4()
+        //Server Connection Info
+        private int ConnectedClients = 0;
+        private static readonly int MaxClientConnections = 10;
+
+        // Server Simple Info
+        public IPAddress GetServerIp() { return LocalAddr; }
+        public int GetServerPort() { return Port; }
+        public int GetConnectedClients() { return ConnectedClients; }
+
+
+        // Server Init
+        private static IPAddress GetServerIPv4() // Get Server Addr if cant get local will just get any which is just 0.0.0.0 bit weird why it does tha
         {
             foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
             {
@@ -41,7 +93,6 @@ class ServerTCPApp
             }
             return IPAddress.Any;
         }
-
         public bool InitServer()
         {
             if (Server == null)
@@ -61,6 +112,14 @@ class ServerTCPApp
             }
             return false;
         }
+        public void ServerShutDown()
+        {
+            if (ServerState)
+            {
+                ToggleServer();
+                ConnectedClients = 0;
+            }
+        }
 
         private static void ToggleServer()
         {
@@ -72,8 +131,23 @@ class ServerTCPApp
             }
             else
             {
-                Server.Stop();
-                Console.WriteLine("Server stopped on {0}:{1}", LocalAddr, Port);
+                try
+                {
+                    Server.Stop();
+                }
+                catch (ObjectDisposedException)
+                {
+                    // do nothing will happen 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error stopping server: {0}", ex.Message);
+                }
+                finally
+                {
+                    Server = null;
+                    Console.WriteLine("Server stopped on {0}:{1}", LocalAddr, Port);
+                }
             }
         }
 
@@ -84,8 +158,14 @@ class ServerTCPApp
                 try
                 {
                     var client = await Server.AcceptTcpClientAsync();
-                    Console.WriteLine("Client connected.");
+                    if (!ServerState) break; // Exit if server is no longer running
+
+                    ConnectedClients++;
                     _ = Task.Run(() => HandleClientAsync(client));
+                }
+                catch (ObjectDisposedException)
+                {
+                    break;
                 }
                 catch (Exception e)
                 {
@@ -94,11 +174,11 @@ class ServerTCPApp
             }
         }
 
+        // Handles packets sent to server along with how to deal with them msgs / pings / certain disconects
         private async Task HandleClientAsync(TcpClient client)
         {
             IPAddress clientIP = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
             int clientPort = ((IPEndPoint)client.Client.RemoteEndPoint).Port;
-            Console.WriteLine($"Handling client {clientIP}:{clientPort}");
 
             try
             {
@@ -112,13 +192,12 @@ class ServerTCPApp
                         while (headerBytesRead < headerBuffer.Length)
                         {
                             int bytesRead = await stream.ReadAsync(headerBuffer, headerBytesRead, headerBuffer.Length - headerBytesRead);
-                            if (bytesRead == 0) break; // End of stream
+                            if (bytesRead == 0) break;
                             headerBytesRead += bytesRead;
                         }
 
                         if (headerBytesRead < ProtocolConsts.HeaderSize)
                         {
-                            Console.WriteLine("Error: Incomplete header read.\n Client lost: {0}:{1}", clientIP, clientPort);
                             break;
                         }
 
@@ -133,7 +212,7 @@ class ServerTCPApp
                             while (payloadBytesRead < payload.Length)
                             {
                                 int bytesRead = await stream.ReadAsync(payload, payloadBytesRead, payload.Length - payloadBytesRead);
-                                if (bytesRead == 0) break; // End of stream
+                                if (bytesRead == 0) break;
                                 payloadBytesRead += bytesRead;
                             }
 
@@ -144,22 +223,27 @@ class ServerTCPApp
                             }
                         }
 
-                        // Process message
                         switch (messageType)
                         {
                             case ProtocolConsts.TypePing:
-                                Console.WriteLine("Received Ping from client");
-
                                 byte[] responseBuffer = new byte[ProtocolConsts.HeaderSize];
                                 responseBuffer[0] = ProtocolConsts.TypePing;
                                 BitConverter.GetBytes(0).CopyTo(responseBuffer, 1);
-
                                 await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
-                                Console.WriteLine("Ping response sent.");
                                 break;
                             case ProtocolConsts.TypeData:
                                 string data = Encoding.ASCII.GetString(payload);
-                                Console.WriteLine($"Message from {clientIP}:{clientPort}: {data}");
+                                string message = $"Message from {clientIP}:{clientPort}: {data}";
+
+                                lock (RecentMessages)
+                                {
+                                    if (RecentMessages.Count >= 3)
+                                    {
+                                        RecentMessages.RemoveAt(0);
+                                    }
+                                    RecentMessages.Add(message);
+                                }
+                                Console.WriteLine(message);
                                 break;
                             case ProtocolConsts.TypeOther:
                                 // Handle other message types if needed
@@ -173,22 +257,114 @@ class ServerTCPApp
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error handling client: {ex.Message}");
+                Console.WriteLine("Forceful disconnect by client {0}:{1}", clientIP, clientPort);
             }
             finally
             {
                 client.Close();
-                Console.WriteLine($"Client {clientIP}:{clientPort} disconnected.");
+                ConnectedClients--;
+                Console.WriteLine("Client {0}:{1} disconnected.", clientIP, clientPort);
+            }
+        }
+    }
+
+    class ServerUI : ServerTCPHandler
+    {
+        ConsoleKeyInfo key;
+        int index = 1; // option currently being selected 
+        bool isSelected = false;
+        string GreenColour = "\u001b[94m";
+        bool bExit = false;
+
+        bool[] toggledOptions = new bool[4];// array to keep track of the toggled stuff in ui
+
+        private void Header()
+        {
+            Console.WriteLine("Bean Server Console UI - Made By Le Monke");
+            Console.WriteLine("Use Arrow Keys to navigate UI and use Enter/Return to select.\n");
+        }
+
+        public void Main()
+        {
+            while (!bExit)
+            {
+                Console.Clear();
+
+                (int Left, int Top) = Console.GetCursorPosition();
+                Console.SetCursorPosition(Left, Top);
+
+                Header();
+
+                // Display options with toggle state and server status
+                Console.WriteLine($"{(index == 1 ? GreenColour : "")}Start Server{(toggledOptions[0] ? " \u001b[92mX - Server started on " + GetServerIp() + ":" + GetServerPort()  + " : Connected Clients " + GetConnectedClients(): "")}\u001b[0m");
+                Console.WriteLine($"{(index == 2 ? GreenColour : "")}List Clients{(toggledOptions[1] ? " \u001b[92mX" : "")}\u001b[0m");
+                Console.WriteLine($"{(index == 3 ? GreenColour : "")}Shutdown Server{(toggledOptions[2] ? " \u001b[92mX" : "")}\u001b[0m");
+                Console.WriteLine($"{(index == 4 ? GreenColour : "")}Exit{(toggledOptions[3] ? " \u001b[92mX" : "")}\u001b[0m");
+
+                // Display recent messages
+                lock (RecentMessages)
+                {
+                    foreach (var message in RecentMessages)
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+
+                key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        index = (index == 4) ? 1 : index + 1;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        index = (index == 1) ? 4 : index - 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        isSelected = true;
+                        HandleSelection();
+                        isSelected = false;
+                        break;
+                }
+            }
+        }
+
+        private void HandleSelection()
+        {
+            switch (index)
+            {
+                case 1:
+                    if (!toggledOptions[0])
+                    {
+                        InitServer();
+                        toggledOptions[0] = true;
+                        toggledOptions[2] = false;
+                    }
+                    break;
+                case 2:
+                    //ListClients(); - not done yet 
+                    toggledOptions[1] = !toggledOptions[1];
+                    break;
+                case 3:
+                    if (toggledOptions[0])
+                    {
+                        ServerShutDown();
+                        toggledOptions[2] = true;
+                        toggledOptions[0] = false;
+                    }
+                    break;
+                case 4:
+                    bExit = true;
+                    break;
             }
         }
     }
 
     private static void Main(string[] args)
     {
-        var serverHandler = new ServerTCPHandler();
-        serverHandler.InitServer();
+        //Console Ui
+        ServerUI balls = new ServerUI();
 
-        Console.WriteLine("Press Enter to exit...");
-        Console.ReadLine();
+        balls.Main();
     }
 }
